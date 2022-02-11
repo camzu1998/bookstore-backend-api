4e1cac34c5f3abe9b2492cb4873c9a66ca353958
camzu1998 <kamillanger4@gmail.com>
2022-02-11 22:05:40 +0100

    Database migrations

---
+++ database/migrations/2022_02_11_182119_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('author_id');
+            $table->string('name');
+            $table->string('type');
+            $table->integer('quantity')->default(0);
+            $table->float('price', 8, 2)->default(0);
+            $table->timestamps();
+
+            $table->index(['author_id', 'name']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2022_02_11_191451_create_cart_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cart_id');
+            $table->integer('book_id');
+            $table->string('type');
+            $table->float('price', 8, 2);
+            $table->timestamps();
+
+            $table->index(['cart_id', 'book_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_products');
+    }
+};
